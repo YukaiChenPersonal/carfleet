17fd97d2765965e027c55214a4f86050a6e08f50
YukaiChenPersonal <115656605+YukaiChenPersonal@users.noreply.github.com>
2022-10-13 17:06:52 -0400

    Add files via upload

--- battery.py
+++ battery.py
@@ -1,24 +1,15 @@
-from abc import ABC, abstractmethod
-
-class Battery(ABC):
-    
-    @abstractmethod
-    def battery_type(self):
-        pass
-
-
-class SpindlerBattery(Battery):
-    def battery_type(self, cur_date, last_serv_date):
+class spindler_battery:
+    def __init__(self, cur_date, last_serv_date):
         self.cur_date = cur_date
         self.last_serv_date = last_serv_date
 
-    def battery_should_be_serviced(self):
+    def need_serv(self):
         return self.cur_date - self.last_serv_date > 2 * 365
 
-class NubbinBattery(Battery):
-    def battery_type(self, cur_date, last_serv_date):
+class nubbin_battery:
+    def __init__(self, cur_date, last_serv_date):
         self.cur_date = cur_date
         self.last_serv_date = last_serv_date
 
-    def battery_should_be_serviced(self):
+    def need_serv(self):
         return self.cur_date - self.last_serv_date > 4 * 365
--- engine.py
+++ engine.py
@@ -1,34 +1,25 @@
-from abc import ABC, abstractmethod
-
-class Engine(ABC):
-    
-    @abstractmethod
-    def engine_type(self):
-        pass
-
-
-class capulet_engine(Engine):
-    def engine_type(self, current_mileage, last_service_mileage):
+class capulet_engine:
+    def __init__(self, current_mileage, last_service_mileage):
         self.current_mileage = current_mileage
         self.last_service_mileage = last_service_mileage
 
-    def engine_should_be_serviced(self):
+    def need_serv(self):
         return self.current_mileage - self.last_service_mileage > 30000
 
-class SternmanEngine(Engine):
-    def engine_type(self, warning_light_is_on):
+class sternman_engine:
+    def __init__(self, warning_light_is_on):
         self.warning_light_is_on = warning_light_is_on
 
-    def engine_should_be_serviced(self):
+    def need_serv(self):
         if self.warning_light_is_on:
             return True
         else:
             return False
 
-class WilloughbyEngine(Engine):
-    def engine_type(self, current_mileage, last_service_mileage):
+class willoughby_engine:
+    def __init__(self, current_mileage, last_service_mileage):
         self.current_mileage = current_mileage
         self.last_service_mileage = last_service_mileage
 
-    def engine_should_be_serviced(self):
+    def need_serv(self):
         return self.current_mileage - self.last_service_mileage > 60000
